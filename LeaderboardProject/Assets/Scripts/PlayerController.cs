c83c2cc06dca353190cbb9d92403ca66898027c3
Ace-03 <66760646+Ace-03@users.noreply.github.com>
2023-11-10 18:11:30 -0700

    created new project

---
+++ LeaderboardProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rig;
+
+    private float startTime;
+    private float timeTaken;
+
+    private int collectablesPicked;
+    public int maxCollectables = 10;
+    private bool isPlaying;
+
+    void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    void Begin()
+    {
+        startTime = Time.time;
+        isPlaying = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * speed;
+        float z = Input.GetAxis("Vertical") * speed;
+
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            collectablesPicked++;
+            Destroy(other.gameObject);
+
+            if (collectablesPicked == maxCollectables)
+                End();
+        }
+    }
+
+    void End()
+    {
+        timeTaken = Time.time - startTime;
+        isPlaying = false;
+    }
+}
